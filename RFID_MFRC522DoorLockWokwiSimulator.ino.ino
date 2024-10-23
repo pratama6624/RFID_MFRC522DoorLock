9185d1388467f28e520d5fd38c06e069ce1ec4ec
pratama6624 <scyoung6624@gmail.com>
2024-10-23 22:48:19 +0700

    Adding mock files for simulation directly on the WOKWI website

---
+++ RFID_MFRC522DoorLockWokwiSimulator.ino.ino
@@ -0,0 +1,165 @@
+/*
+ * --------------------------------------------------------------------------------------------------------------------
+ * Example sketch/program showing how to read data from a PICC to serial.
+ * --------------------------------------------------------------------------------------------------------------------
+ * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
+ * 
+ * Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
+ * Reader on the Arduino SPI interface.
+ * 
+ * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
+ * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
+ * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
+ * will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
+ * when removing the PICC from reading distance too early.
+ * 
+ * If your reader supports it, this sketch/program will read all the PICCs presented (that is: multiple tag reading).
+ * So if you stack two or more PICCs on top of each other and present them to the reader, it will first output all
+ * details of the first and then the next PICC. Note that this may take some time as all data blocks are dumped, so
+ * keep the PICCs at reading distance until complete.
+ * 
+ * @license Released into the public domain.
+ * 
+ * Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ *
+ * More pin layouts for other boards can be found here: https://github.com/miguelbalboa/rfid#pin-layout
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal.h> // Library untuk LCD
+
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
+
+#define GREEN_LED_PIN 7
+#define RED_LED_PIN 6
+#define BUZZER_PIN 8
+
+byte validUID[7] = {0x05, 0x83, 0x0C, 0x5A, 0xBA, 0xD1, 0x00};
+
+bool convertInputToUID(String input, byte *uid); 
+
+void setup() {
+	Serial.begin(9600);		// Initialize serial communications with the PC
+	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+	SPI.begin();			// Init SPI bus
+	mfrc522.PCD_Init();		// Init MFRC522
+	delay(4);				// Optional delay. Some board do need more time after init to be ready, see Readme
+	// mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
+	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+
+  lcd.begin(16, 2);        // Inisialisasi LCD 16x2
+  delay(100); 
+  lcd.clear();
+  lcd.print("Scan card");
+
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  digitalWrite(GREEN_LED_PIN, LOW);
+  digitalWrite(RED_LED_PIN, LOW);
+  digitalWrite(BUZZER_PIN, LOW);
+}
+
+bool convertInputToUID(String input, byte *uid) {
+  if (input.length() != 14) {
+    return false;
+  }
+
+  for (byte i = 0; i < 7; i++) {
+    String byteString = input.substring(i * 2, i * 2 + 2);
+    uid[i] = (byte) strtol(byteString.c_str(), NULL, 16);
+  }
+
+  return true;
+}
+
+void loop() {
+	// // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+	// if ( ! mfrc522.PICC_IsNewCardPresent()) {
+	// 	return;
+	// }
+
+	// // Select one of the cards
+	// if ( ! mfrc522.PICC_ReadCardSerial()) {
+	// 	return;
+	// }
+
+	// Dump debug info about the card; PICC_HaltA() is automatically called
+	// mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+
+  Serial.print("UID tag: ");
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+
+  Serial.println();
+
+  // Simulasi dengan WOKWI
+  if (Serial.available() > 0) {
+    String inputUID = Serial.readStringUntil('\n');
+    inputUID.trim();
+    byte simulatedUID[7];
+
+    if (convertInputToUID(inputUID, simulatedUID)) {
+      // Setelah UID diinputkan, lakukan pemeriksaan validasi
+        if (checkUID(simulatedUID, 7)) {
+          Serial.println("Valid Card, turning GREEN LED on.");
+          digitalWrite(GREEN_LED_PIN, HIGH);
+          digitalWrite(RED_LED_PIN, LOW);
+          tone(BUZZER_PIN, 1000);
+          lcd.clear();
+          lcd.print("Welcome Home Bro");
+          delay(5000);
+          lcd.clear();
+          lcd.print("Scan card");
+          digitalWrite(GREEN_LED_PIN, LOW);
+          noTone(BUZZER_PIN);
+        } else {
+          Serial.println("Invalid Card, turning RED LED on.");
+          digitalWrite(GREEN_LED_PIN, LOW);
+          digitalWrite(RED_LED_PIN, HIGH);
+          tone(BUZZER_PIN, 10);
+          lcd.clear();
+          lcd.print("Who are you dowg..?");
+          delay(5000);
+          lcd.clear();
+          lcd.print("Scan card");
+          digitalWrite(RED_LED_PIN, LOW);
+          noTone(BUZZER_PIN);
+        }
+      } else {
+        Serial.println("Invalid UID format");
+      }
+  }
+
+  mfrc522.PICC_HaltA();
+}
+
+bool checkUID(byte *uid, byte uidSize) {
+  if (uidSize != 7) {
+    return false;
+  }
+  for (byte i = 0; i < uidSize; i++) {
+    if (uid[i] != validUID[i]) {
+      return false;
+    }
+  }
+  return true;
+}
